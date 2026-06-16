8c96dcbff7535ad976ba7871d7ab460ddf6dd84b
francesca-golzio <francescagolzio@gmail.com>
2026-06-16 20:27:47 +0200

    import Bootstrap

--- index.php
+++ index.php
@@ -48,6 +48,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Import Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   <title>🏨 PHP Hotel</title>
 </head>
 <body>
@@ -57,6 +59,12 @@
   </header>
   
   <main>
+
+
+    
+
+
+
 
     <?php
 
@@ -73,6 +81,18 @@
       }
     
     ?>
+
+    <!-- Import Bootstrap -->
+    <script 
+    src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" 
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" 
+    crossorigin="anonymous"></script>
+    <!-- __ ⚠️ DA TOGLIERE SE NON SERVE __ 
+     <script 
+     src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" 
+     integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" 
+     crossorigin="anonymous"></script>
+      -->
 
   </main>
 
