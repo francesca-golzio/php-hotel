6153a5ed9ca42242c48596bbc0e6ca6dbd7b077d
francesca-golzio <francescagolzio@gmail.com>
2026-07-01 10:50:38 +0200

    add parking filter

--- index.php
+++ index.php
@@ -1,46 +1,50 @@
 <?php
-// Store variables
+  // Store variables
 
-    $hotels = [
+  $hotels = [
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
+      [
+          'name' => 'Hotel Belvedere',
+          'description' => 'Hotel Belvedere Descrizione',
+          'parking' => true,
+          'vote' => 4,
+          'distance_to_center' => 10.4
+      ],
+      [
+          'name' => 'Hotel Futuro',
+          'description' => 'Hotel Futuro Descrizione',
+          'parking' => true,
+          'vote' => 2,
+          'distance_to_center' => 2
+      ],
+      [
+          'name' => 'Hotel Rivamare',
+          'description' => 'Hotel Rivamare Descrizione',
+          'parking' => false,
+          'vote' => 1,
+          'distance_to_center' => 1
+      ],
+      [
+          'name' => 'Hotel Bellavista',
+          'description' => 'Hotel Bellavista Descrizione',
+          'parking' => false,
+          'vote' => 5,
+          'distance_to_center' => 5.5
+      ],
+      [
+          'name' => 'Hotel Milano',
+          'description' => 'Hotel Milano Descrizione',
+          'parking' => true,
+          'vote' => 2,
+          'distance_to_center' => 50
+      ],
 
-    ];
+  ];
 
+  // set the parking_requested variable to the default value
+  $parking_requested = false;
+
+    
 ?>
 
 <!DOCTYPE html>
@@ -60,6 +64,22 @@
   
   <main>
 
+    <!-- Filtering Form -->
+    <div class="container-fluid">
+      <form action="" method="GET" class="d-flex gap-2">
+
+        <div class="form-control">
+          <input type="checkbox" name="parking" id="parking">
+          <label for="parking">Parcheggio</label>
+        </div>
+
+        <button type="submit">FILTRA</button>
+
+      </form>
+    </div>
+
+
+    <!-- Hotels table -->
     <div class="container-fluid">
       <table class="table">
         <thead>
@@ -71,28 +91,38 @@
             <th scope="col">Distance to Center</th>
           </tr>
         </thead>
-
         <tbody>
-        <?php
-        // Cycle inside the hotels array
-          // for each hotel of the hotels list
-          foreach ($hotels as $hotel) {
-        ?>
-        <tr>
-          <td><?php echo $hotel['name'] ?></td>
-          <td><?php echo $hotel['description'] ?></td>
-          <td>
-            <?php 
-              echo $hotel['parking'] ? 'presente' : 'assente';
-            ?>
-          </td>
-          <td><?php echo $hotel['vote'] ?></td>
-          <td><?php echo $hotel['distance_to_center'] ?></td>
-        </tr>
-        <?php
-          }
-        ?>
-        
+          <?php
+          // Cycle inside the hotels array
+            // for each hotel of the hotels list
+            foreach ($hotels as $hotel) {
+
+              // 🅿️ Handle parking filter
+              // check IF the parking is requested 
+              if (isset($_GET['parking']) && $_GET['parking'] == 'on') {
+
+                // IF so, update the parking_requested variable
+                $parking_requested = true;
+              }
+              // IF parking_requested (is true) 
+              if ($parking_requested) {
+                // BUT the hotel doesn't have it, THEN skip current hotel
+                if (!$hotel['parking']) {
+                  continue;
+                }
+              }
+
+          ?>
+          <tr>
+            <td><?php echo $hotel['name'] ?></td>
+            <td><?php echo $hotel['description'] ?></td>
+            <td><?php echo $hotel['parking'] ? 'presente' : 'assente'; ?></td>
+            <td><?php echo $hotel['vote'] ?></td>
+            <td><?php echo $hotel['distance_to_center'] ?></td>
+          </tr>
+          <?php
+            }
+          ?>        
         </tbody>
 
 
